cf4a4563cf716130d73c3ab5b8c1b922141de7bd
Jan Nicklaus <jannicklaus33@googlemail.com>
2024-03-24 16:55:49 +0100

    Gesamt-Test

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, request, redirect, session, flash, jsonify, url_for
 from flask_session import Session  # Import für die Session-Erweiterung
-
-app = Flask(__name__)
-
-# Konfigurationen für automatisches Template-Reload, Sessions und Datenbank
+app = Flask(__name__, template_folder='frontend')  # Setze den template_folder auf den 'frontend' Ordner
+# Konfigurationen für automatisches Template-Reload, Sessions und Datenbank, falls wir eine brauchen
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-# festgelegt, dass Session nicht dauerhaft ist
 app.config["SESSION_PERMANENT"] = False
-# Sitzungsinformationen auf dem Dateisystem gespeichert
 app.config["SESSION_TYPE"] = "filesystem"
-# Geheimer Schlüssel für die Session-Sicherheit (notwendig für die Session-Verwaltung)
 Session(app)  # Initialisiert die Session mit der App
-
 @app.route('/')
 def home():
-    return "Legal Prompt"
-
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True, port=5001)  # Startet den Server auf Port 5001 mit aktiviertem Debug-Modus
+
+
+
+
+
+
+
+
